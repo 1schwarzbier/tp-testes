336576372385ff1c3822a5f06cce6cb4d93e33f5
jackson <8601315+1schwarzbier@users.noreply.github.com>
2022-05-22 17:08:13 -0300

    feat(web-api): add property controller
     - add property service interface
     - add property validators
     - add initial tests for validators
     - add README

---
+++ RealEstate/Services/PropertyService/Models/InsertPropertyRequest.cs
@@ -0,0 +1,20 @@
+﻿namespace RealEstate.Services.PropertyService.Models
+{
+    public class InsertPropertyRequest
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Type { get; set; }
+        public string? SaleMode { get; set; }
+        public string? Address { get; set; } = "Address";
+        public decimal Size { get; set; }
+        public string? ContactInfo { get; set; } = "Contact";
+        public decimal Price { get; set; }
+        public int BathroomCount { get; set; }
+        public int RoomCount { get; set; }
+        public int ParkingCount { get; set; }
+        public bool IsActive { get; set; } = true;
+        public bool IsCommercial { get; set; } = false;
+    }
+}
---
+++ RealEstate/Services/PropertyService/Validator/InsertPropertyValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using RealEstate.Services.PropertyService.Models;
+
+namespace RealEstate.Services.PropertyService.Validator;
+
+public class InsertPropertyValidator : AbstractValidator<InsertPropertyRequest>
+{
+    public InsertPropertyValidator()
+    {
+        RuleFor(r => r.Name).NotEmpty();
+        RuleFor(r => r.Description).NotEmpty();
+        RuleFor(r => r.ImageUrl).Must(CustomValidators.LinkMustBeAUri);
+        RuleFor(r => r.Type).Must(CustomValidators.MustBeValidPropertyType)
+            .WithMessage("The specified 'Type' is not valid. The options are (Apartment, House)");
+        RuleFor(r => r.SaleMode).Must(CustomValidators.MustBeValidSaleMode)
+            .WithMessage("The specified 'SaleMode' is not valid. The options are (Sale, Rent)");
+        RuleFor(r => r.Address).NotEmpty();
+        RuleFor(r => r.Size).GreaterThan(0);
+        RuleFor(r => r.ContactInfo).NotEmpty();
+        RuleFor(r => r.Price).GreaterThan(0);
+        RuleFor(r => r.BathroomCount).GreaterThan(0);
+        RuleFor(r => r.RoomCount).GreaterThan(0);
+        RuleFor(r => r.ParkingCount).GreaterThanOrEqualTo(0);
+    }
+}
